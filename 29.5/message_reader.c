81b04b2231627c3de84af818704cb7fbd653763f
noahadash1 <92437002+noahadash1@users.noreply.github.com>
2023-05-31 13:34:10 +0300

    Update message_reader.c

--- 29.5/message_reader.c
+++ 29.5/message_reader.c
@@ -8,6 +8,7 @@
 #include "message_slot.h"
 
 int main(int argc, char const *argv[]) {
+    //printf("start of reader);
     if (argc != 3){
         perror("wrong anount of args");
         exit(1);
@@ -23,10 +24,10 @@ int main(int argc, char const *argv[]) {
     }
     char buffer[BUF_LEN];
     int msLen = read(file, buffer, BUF_LEN);
-    //if (msLen < 0){
-     //   perror("reading message failed");
-     //   exit(1);  
- //   }
+    if (msLen < 0){
+        perror("reading message failed");
+        exit(1);  
+    }
     close(file);
     if (write(1, buffer, msLen) != msLen) {
         perror("writting the massage to stdout faild");
