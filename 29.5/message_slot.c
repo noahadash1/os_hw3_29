f997ad728e99a32331b9c7a32b93199c212e0384
noahadash1 <92437002+noahadash1@users.noreply.github.com>
2023-05-31 16:12:22 +0300

    Update message_slot.c

--- 29.5/message_slot.c
+++ 29.5/message_slot.c
@@ -41,15 +41,15 @@ static long device_ioctl( struct file* file, unsigned int ioctl_command_id, unsi
   if (curChannelMinorNum == NULL){
     printk("it is Null");
   }
-  if(curChannelMinorNum == 4){
-    printk("it is 4");
-  }
-  if(curChannelMinorNum > 4){
-    printk("it is bigger then 4");
-  }
-  if(curChannelMinorNum < 4){
-    printk("it is smaller then 4");
-  }
+//   if(curChannelMinorNum == 4){
+//     printk("it is 4");
+//   }
+//   if(curChannelMinorNum > 4){
+//     printk("it is bigger then 4");
+//   }
+//   if(curChannelMinorNum < 4){
+//     printk("it is smaller then 4");
+//   }
   channelPointer = massageSlotsDeviceFilesList[curChannelMinorNum].first;
   while(channelPointer != NULL) {
     if(channelPointer->ID == ioctl_param){
